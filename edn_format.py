5e8e545298268a7d2f7ea0fe86ccd9ef5f285ad4
Swaroop C H <swaroop@swaroopch.com>
2012-12-21 20:26:26 +0530

    Added basic project and basic test

---
+++ edn_format.py
@@ -0,0 +1,15 @@
+
+import pytz
+import pyrfc3339
+from datetime import datetime
+import uuid
+
+
+def loads(text):
+    obj = text # TODO Parse EDN format
+    return obj
+
+
+def dumps(obj):
+    text = obj # TODO Convert to EDN format
+    return text
---
+++ tests.py
@@ -0,0 +1,31 @@
+
+
+import unittest
+import edn_format as edn
+
+
+class EdnRoundTripTest(unittest.TestCase):
+    def setUp(self):
+        self.edn_literals = ("nil",
+                             "true",
+                             "false",
+                             r"\c",
+                             '"hello world"',
+                             ":keyword",
+                             "symbol",
+                             "123",
+                             "123N",
+                             "32.23",
+                             "32.23M",
+                             '(1 "abc" true :ghi)',
+                             '[1 "abc" true :ghi]',
+                             '{:a 1 "foo" :bar, [1 2 3]}',
+                             '#{:a :b [1 2 3]',
+                             '#myapp/Person {:first "Fred" :last "Mertz',
+                             '#inst "1985-04-12T23:20:50.52Z"',
+                             '#uuid "f81d4fae-7dec-11d0-a765-00a0c91e6bf6"')
+
+
+    def test_round_trips(self):
+        for literal in self.edn_literals:
+            self.assertEqual(literal, edn.dumps(edn.loads(literal)))
