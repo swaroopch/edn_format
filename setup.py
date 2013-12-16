4fd56a8407e1be4c823b66f0ab921cb1371a9972
Andrew Shinohara <andrew.shinohara@invitae.com>
2013-12-16 15:28:39 -0800

    Bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="edn_format",
-      version="0.3.8",
+      version="0.3.9",
       author="Swaroop C H",
       author_email="swaroop@swaroopch.com",
       description="EDN format reader and writer in Python",
