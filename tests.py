498ddc2962c00ff5ab824583bd6d720d61047a55
Swaroop C H <swaroop@swaroopch.com>
2014-01-24 16:35:48 +0530

    PEP8 formatting

--- tests.py
+++ tests.py
@@ -1,9 +1,13 @@
-
 import datetime
 import pytz
-
 import unittest
-from edn_format import edn_lex, edn_parse, loads, dumps, Keyword, TaggedElement, add_tag
+from edn_format import edn_lex, \
+    edn_parse, \
+    loads, \
+    dumps, \
+    Keyword, \
+    TaggedElement, \
+    add_tag
 
 
 class ConsoleTest(unittest.TestCase):
@@ -11,7 +15,7 @@ class ConsoleTest(unittest.TestCase):
         is_exception = False
         try:
             loads("[1 true nil]")
-        except AttributeError as x:
+        except AttributeError:
             is_exception = True
         self.assertFalse(is_exception)
 
@@ -20,7 +24,6 @@ class EdnTest(unittest.TestCase):
     def check_lex(self, expected_output, actual_input):
         self.assertEqual(expected_output, str(list(edn_lex.lex(actual_input))))
 
-
     def test_lexer(self):
         self.check_lex("[LexToken(NIL,None,1,0)]",
                        "nil")
@@ -28,8 +31,11 @@ class EdnTest(unittest.TestCase):
                        "true")
         self.check_lex("[LexToken(INTEGER,123,1,0)]",
                        "123")
-        self.check_lex("[LexToken(INTEGER,456,1,0), LexToken(NIL,None,1,4), LexToken(BOOLEAN,False,1,8)]",
-                       "456 nil false")
+        self.check_lex(
+            "[LexToken(INTEGER,456,1,0), " +
+            "LexToken(NIL,None,1,4), " +
+            "LexToken(BOOLEAN,False,1,8)]",
+            "456 nil false")
         self.check_lex("[LexToken(CHAR,'c',1,0)]",
                        r"\c")
         self.check_lex("[LexToken(KEYWORD,Keyword(abc),1,0)]",
@@ -40,7 +46,8 @@ class EdnTest(unittest.TestCase):
                        "; a comment")
         self.check_lex("[LexToken(KEYWORD,Keyword(abc),1,0)]",
                        ":abc ; a comment")
-        self.check_lex("[LexToken(TAG,'inst',1,0), LexToken(STRING,'1985-04-12T23:20:50.52Z',1,6)]",
+        self.check_lex("[LexToken(TAG,'inst',1,0), " +
+                       "LexToken(STRING,'1985-04-12T23:20:50.52Z',1,6)]",
                        '#inst "1985-04-12T23:20:50.52Z"')
         self.check_lex("[LexToken(SYMBOL,Symbol(abc),1,0)]",
                        "abc")
@@ -53,10 +60,8 @@ class EdnTest(unittest.TestCase):
         self.check_lex("[LexToken(SYMBOL,Symbol(true.),1,0)]",
                        "true.")
 
-
     def check_parse(self, expected_output, actual_input):
         self.assertEqual(expected_output, edn_parse.parse(actual_input))
-
 
     def test_parser(self):
         self.check_parse(1,
@@ -76,16 +81,16 @@ class EdnTest(unittest.TestCase):
         self.check_parse([Keyword("abc"), 1, True, None],
                          "[:abc 1 true nil]")
         self.check_parse((Keyword("abc"), 1, True, None),
-            "(:abc 1 true nil)")
+                         "(:abc 1 true nil)")
         self.check_parse(tuple(), "()")
         self.check_parse(set(), "#{}")
         self.check_parse({}, "{}")
         self.check_parse([], "[]")
-        self.check_parse({"a" : [1, 2, 3]},
+        self.check_parse({"a": [1, 2, 3]},
                          '{"a" [1 2 3]}')
-        self.check_parse(datetime.datetime(2012, 12, 22, 19, 40, 18, 0, tzinfo=pytz.utc),
-                        '#inst "2012-12-22T19:40:18Z"')
-
+        self.check_parse(
+            datetime.datetime(2012, 12, 22, 19, 40, 18, 0, tzinfo=pytz.utc),
+            '#inst "2012-12-22T19:40:18Z"')
 
     def check_dump(self, expected_output, actual_input):
         if isinstance(expected_output, list):
@@ -93,15 +98,15 @@ class EdnTest(unittest.TestCase):
         else:
             self.assertEqual(expected_output, dumps(actual_input))
 
-
     def test_dump(self):
         self.check_dump("#{1 2 3}",
                         {1, 2, 3})
-        self.check_dump(['{:bar [1 2 3] "foo" :gone :a 1}', '{:a 1 "foo" :gone :bar [1 2 3]}'],
-                        {Keyword("a") : 1,
-                         "foo" : Keyword("gone"),
-                         Keyword("bar") : [1, 2, 3]})
-
+        self.check_dump(
+            ['{:bar [1 2 3] "foo" :gone :a 1}',
+             '{:a 1 "foo" :gone :bar [1 2 3]}'],
+            {Keyword("a"): 1,
+             "foo": Keyword("gone"),
+             Keyword("bar"): [1, 2, 3]})
 
     def test_round_trip_conversion(self):
         EDN_LITERALS = [
@@ -115,7 +120,10 @@ class EdnTest(unittest.TestCase):
             ["123.2", "123.2"],
             ["+32.23M", "32.23M"],
             ["3.23e10", "32300000000.0"],
-            ['#{:a (1 2 3) :b}', ['#{:a (1 2 3) :b}', '#{(1 2 3) :a :b}', '#{:a :b (1 2 3)}', '#{:b :a (1 2 3)}']]
+            ['#{:a (1 2 3) :b}', ['#{:a (1 2 3) :b}',
+                                  '#{(1 2 3) :a :b}',
+                                  '#{:a :b (1 2 3)}',
+                                  '#{:b :a (1 2 3)}']]
         ]
 
         for literal in EDN_LITERALS:
@@ -127,7 +135,6 @@ class EdnTest(unittest.TestCase):
                 self.assertIn(step3, literal[1])
             else:
                 self.assertEqual(literal[1], step3)
-
 
     def test_round_trip_same(self):
         EDN_LITERALS = (
@@ -169,10 +176,14 @@ class EdnTest(unittest.TestCase):
         class TagDate(TaggedElement):
             def __init__(self, value):
                 self.name = 'date'
-                self.value = datetime.datetime.strptime(value, "%d/%m/%Y").date()
+                self.value = datetime.datetime.strptime(
+                    value,
+                    "%d/%m/%Y").date()
 
             def __str__(self):
-                return '#{} "{}"'.format(self.name, self.value.strftime("%d/%m/%Y"))
+                return '#{} "{}"'.format(
+                    self.name,
+                    self.value.strftime("%d/%m/%Y"))
 
         add_tag('date', TagDate)
 
