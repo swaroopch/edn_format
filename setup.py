4877af19adca0b78be6ea28e01ed149d566773b5
Swaroop <swaroop@swaroopch.com>
2015-05-25 12:19:10 -0700

    Bumping up version number and dependencies

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="edn_format",
-      version="0.5.4",
+      version="0.5.5",
       author="Swaroop C H",
       author_email="swaroop@swaroopch.com",
       description="EDN format reader and writer in Python",
       long_description=open('README.md').read(),
       url="https://github.com/swaroopch/edn_format",
       install_requires=[
-          "pytz==2013.9",
+          "pytz==2015.4",
           "pyRFC3339==0.2",
-          "ply==3.4",
+          "ply==3.6",
       ],
       license="Apache 2.0",
       packages=['edn_format'],
