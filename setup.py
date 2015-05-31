bae417882c13797ad4124977e2ea6bd262f0e195
Swaroop C H <swaroop@swaroopch.com>
2015-05-31 08:19:39 -0700

    Bumping up to 0.5.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="edn_format",
-      version="0.5.5",
+      version="0.5.6",
       author="Swaroop C H",
       author_email="swaroop@swaroopch.com",
       description="EDN format reader and writer in Python",
