614d78ab2bea5195896bc5fbb51f9bf55b0e86e7
Swaroop C H <swaroop@swaroopch.com>
2015-06-08 13:59:09 -0700

    Add Python 3 classifier

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="edn_format",
-      version="0.5.6",
+      version="0.5.7",
       author="Swaroop C H",
       author_email="swaroop@swaroopch.com",
       description="EDN format reader and writer in Python",
@@ -18,11 +18,14 @@ setup(name="edn_format",
       ],
       license="Apache 2.0",
       packages=['edn_format'],
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           'Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
           'Topic :: Software Development',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
           'Operating System :: OS Independent',
       ])
