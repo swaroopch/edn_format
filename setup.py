116fd10aad3a81382b57a883c0e5bef58956f2c7
Felipe Sabino <sabino@users.noreply.github.com>
2018-07-02 18:39:00 -0300

    Bump edn_format (#3)
    * Convert requirements from exact to minimum
    
    Fixes #40
    
    * Bump version to 0.5.13
    
    * pyrfc version

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="edn_format",
-      version="0.5.15-nu",
+      version="0.5.16-nu",
       author="Swaroop C H",
       author_email="swaroop@swaroopch.com",
       description="EDN format reader and writer in Python",
       long_description=open('README.md').read(),
       url="https://github.com/swaroopch/edn_format",
       install_requires=[
-          "pytz",
-          "pyRFC3339==1.0",
-          "ply==3.6",
+          "pytz>=2016.10",
+          "pyRFC3339>=1.0,<2",
+          "ply>=3.10,<4",
           "pytest",
           "pytest-xdist"
       ],
