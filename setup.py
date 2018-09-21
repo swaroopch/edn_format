c14529e215492e999446c4ad76078bd0a334020e
Swaroop C H <swaroop@swaroopch.com>
2018-09-21 13:04:18 -0700

    Update setup.py to 0.6.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name="edn_format",
-      version="0.6.0",
+      version="0.6.1",
       author="Swaroop C H",
       author_email="swaroop@swaroopch.com",
       description="EDN format reader and writer in Python",
